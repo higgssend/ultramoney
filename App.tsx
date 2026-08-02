5ef7f7b10a2c5e459551e90120ec2e7dd7bd406b
UltraMoney <elevateenterprisebrands@gmail.com>
2026-08-02 06:33:36 -0400

    Set login as default route in Tauri app

--- App.tsx
+++ App.tsx
@@ -140,7 +140,7 @@ const AppContent: React.FC = () => {
         <div className={`${!isFullScreenPage && currentUser ? 'p-4 md:p-8 pb-24 md:pb-8' : ''}`}>
             <Routes>
             {/* Public Routes */}
-            <Route path="/" element={<LandingPage />} />
+            <Route path="/" element={"__TAURI_INTERNALS__" in window ? <Navigate to="/login" replace /> : <LandingPage />} />
             <Route path="/login" element={<PublicOnlyRoute><Login /></PublicOnlyRoute>} />
             <Route path="/login/:slug" element={<PublicOnlyRoute><CompanyLogin /></PublicOnlyRoute>} />
             <Route path="/login-staff" element={<PublicOnlyRoute><EmployeeLogin /></PublicOnlyRoute>} />
